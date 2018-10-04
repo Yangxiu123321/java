e19fdf195f2a72ccce9d7222c8ed4be4fc80a9fd
DOOM\Yang <13390560336@163.com>
2018-10-04 14:54:25 +0800

    方法

--- src/com/Jsample/Test.java
+++ src/com/Jsample/Test.java
@@ -1,15 +1,49 @@
 package com.jsample;
 
+
 public class Test {
+    //设计方法的原则：方法的本意是功能块，就是实现某个功能的语句块的集合。   我们设计方法的时候，最好保持方法的原子性，就是一个方法只完成1个功能，这样利于我们后期的扩展。
+    public static void test01(int a) {
+        int oddSum = 0; // 用来保存奇数的和
+        int evenSum = 0; // 用来存放偶数的和
+        for (int i = 0; i <= a; i++) {
+            if (i % 2 != 0) {
+                oddSum += i;
+            } else {
+                evenSum += i;
+            }
+
+        }
+        System.out.println("奇数的和：" + oddSum);
+        System.out.println("偶数的和：" + evenSum);
+    }
+
+    public static void test02(int a,int b,int c){
+        for (int j = 1; j <= a; j++) {
+            if (j % b == 0) {
+                System.out.print(j + "\t");
+            }
+            if (j % (b * c) == 0) {
+                System.out.println();
+            }
+        }
+    }
+
+    public static int add(int a,int b){
+        int sum = a+b;
+        if(a==3){
+            return 0;    //return 两个作用：结束方法的运行、返回值。
+        }
+        System.out.println("输出");
+        return sum;
+    }
+
     public static  void main(String[] args)
     {
-        int money = 1000000000;  //10亿
-        int years = 20;
-        int total = money*years;   //返回的是负数
-        long total1 = money*years;  //返回的仍然是负数。默认是int，因此结果会转成int值，再转成long。但是已经发生了数据丢失
-        long total2 = money*((long)years);   //先将一个因子变成long，整个表达式发生提升。全部用long来计算。
-        System.out.println(total);
-        System.out.println(total1);
-        System.out.println(total2);
+        test01(1000);
+        test02(100,6,3);  //1-100之间，可以被6整除，每行输出3个。
+        System.out.println("###########");
+        int s = add(3,5);
+        System.out.println(s);
     }
 }
