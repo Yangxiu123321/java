924e0fe3a2bfd3eb20ed16d26fa0cac9017b1195
DOOM\Yang <13390560336@163.com>
2018-10-04 13:32:43 +0800

    类型提升

--- src/com/Jsample/Test.java
+++ src/com/Jsample/Test.java
@@ -3,22 +3,13 @@ package com.jsample;
 public class Test {
     public static  void main(String[] args)
     {
-        int a = 10;
-        int a2 = 010;
-        int a3 = 0xf;
-        // byte b = 200;
-        // System.out.println(b);
-        System.out.println(a);
-        System.out.println(a2);
-        System.out.println(a3);
-        System.out.println(Integer.toBinaryString(a));
-        System.out.println(Integer.toOctalString(a));
-        System.out.println(Integer.toHexString(a));
-        int a5 = 10;
-        long a6 = 200;
-        byte b2 = 100;   //如果数据的大小没有超过byte/short/char的表述范围，则可以自动转型。
-        long  a7 = 11123213232L;
-        long l = 3;
-        long l2 = l+3;    //L问题
+        int money = 1000000000;  //10亿
+        int years = 20;
+        int total = money*years;   //返回的是负数
+        long total1 = money*years;  //返回的仍然是负数。默认是int，因此结果会转成int值，再转成long。但是已经发生了数据丢失
+        long total2 = money*((long)years);   //先将一个因子变成long，整个表达式发生提升。全部用long来计算。
+        System.out.println(total);
+        System.out.println(total1);
+        System.out.println(total2);
     }
 }
